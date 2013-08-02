5d7def4b4be50c26aebc19b619a03a244749c137
neetin <neetin@regeint.(none)>
2013-08-02 10:50:52 +0545

    adds default route to album

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Gallery::Application.routes.draw do
   resources :albums
-
+  root 'albums#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
